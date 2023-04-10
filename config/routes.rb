4bfb78e3963d3c2cb51a5d76bbebd4a38dabbac2
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-04-10 12:55:23 +0000

    updata

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  post 'lists' => 'lists#create'
   get '/top' => 'homes#top'
+  post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
